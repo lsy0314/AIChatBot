3a09c2da38121f973b069575eebf9eb29ea23e28
Suweon Lim <lsy0314@gmail.com>
2018-05-22 18:43:31 +0900

    Add test script to check tensorflow software
    Signed-off-by: Suweon Lim <lsy0314@gmail.com>

---
+++ test-tf-setup/tf-hello.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python
+# Python
+import tensorflow as tf
+hello = tf.constant('Hello, TensorFlow!')
+sess = tf.Session()
+print(sess.run(hello))
---
+++ test-tf-setup/tf-version.py
@@ -0,0 +1,3 @@
+#!/usr/bin/env python
+import tensorflow as tf
+print(tf.__version__)
